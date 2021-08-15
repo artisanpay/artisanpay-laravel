da83da1bab7a6fffc4ec8da5d167853c6b3d6d9e
Gildas Tema <gildastema3@gmail.com>
2021-08-15 13:01:39 +0100

    add test for webhook

--- tests/ChargeHookControllerTest.php
+++ tests/ChargeHookControllerTest.php
@@ -5,19 +5,27 @@ use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase;
 use Artisanpay\Tests\FakeHandleJob;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
+use Artisanpay\Controllers\ChargeHookController;
 
 class ChargeHookControllerTest extends TestCase
 {
+    /** @test */
     public function handle_payment()
     {
         Config::set('artisanpay.dispatcher', \Artisanpay\Tests\FakeHandleJob::class);
+        Route::post('artisanpay/hooks', ChargeHookController::class);
         Bus::fake();
-        $response =  $this->postJson('artisanpay/hooks', [
+        $response =  $this->postJson('/artisanpay/hooks', [
             'id'    => $id = (string) Str::uuid(),
             'status'    => 'success',
             'operator_message' => 'payment ok'
         ]);
+
+        // assert
+        $response->assertSuccessful();
+        Bus::assertDispatched(FakeHandleJob::class);
 
 
     }
