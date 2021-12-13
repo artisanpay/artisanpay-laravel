07433afca07e7720b5213c3e72f4e84b410a25e2
Gildas Tema <gildastema3@gmail.com>
2021-12-13 20:00:11 +0100

    clean

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Artisanpay\Dto\ChargeHookResponse;
-
-require './vendor/autoload.php';
-
-$r = new ChargeHookResponse(['id' => "5" , "message" => "kolo", "operator_message" => "5000"], 'kiko');
-
-dd($r->getId());
